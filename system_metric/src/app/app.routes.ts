2e251e4126576a958b0c2c1905ec3f4dca036532
jiaenkoh45 <jiaenkoh45@gmail.com>
2026-04-25 21:16:56 +0800

    feat: added routes for main pages dashboard, devices and settings

--- system_metric/src/app/app.routes.ts
+++ system_metric/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard.component';
+import { Devices } from './pages/devices/devices.component';
+import { Settings } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: Dashboard },
+    { path: 'devices', component: Devices },
+    { path: 'setting', component: Settings },
+];
+
